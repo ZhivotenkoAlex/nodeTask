40930e91386738876a75ea010704639ee80437db
lenovo025 <ZhivotenkoAlex>
2021-08-25 17:51:39 +0300

    added PUT and DELETE methods

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http")
+const url = require("url")
 const mariadb = require("mariadb")
 
 const pool = mariadb.createPool({
@@ -8,6 +9,21 @@ const pool = mariadb.createPool({
   database: "nodeDB",
 })
 
+async function getItemById(req, res) {
+  let idFromQueryString = url.parse(req.url, true).query.id
+  try {
+    let conn = await pool.getConnection()
+    let rows = await conn.query(
+      `SELECT text FROM todo WHERE id=${idFromQueryString}`
+    )
+
+    res.end(rows[0].text)
+  } catch (err) {
+    res.statusCode = 404
+    res.end(`There are no item with id=${idFromQueryString}`)
+  }
+}
+
 async function AddItemToDB(body) {
   try {
     let conn = await pool.getConnection()
@@ -15,35 +31,66 @@ async function AddItemToDB(body) {
       `INSERT INTO todo (text) VALUES ('${body.title}');`
     )
     res.end("It`s respond from get")
-    console.log("OK")
-  } catch (error) {}
+  } catch (error) {
+    res.statusCode = 404
+    res.end(`Something went wrong`)
+  }
 }
 
-async function getFirstElement(res) {
+async function editElement(body) {
   try {
     let conn = await pool.getConnection()
-    let rows = await conn.query("SELECT text FROM todo WHERE id=1")
-    console.log(rows[0].text)
-    res.end(rows[0].text)
-  } catch (err) {
-    throw err
+    let rows = await conn.query(
+      `UPDATE todo SET text='${body.title}' WHERE id=${body.id}`
+    )
+  } catch (error) {
+    res.statusCode = 404
+    res.end(`There are no item with id=${idFromQueryString}`)
+  }
+}
+
+async function deleteElement(body) {
+  try {
+    let conn = await pool.getConnection()
+    let rows = await conn.query(`DELETE FROM todo WHERE id=${body.id}`)
+  } catch (error) {
+    res.statusCode = 404
+    res.end(`There are no item with id=${idFromQueryString}`)
   }
 }
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "application/json")
+  let body = null
+
   if (req.method === "GET") {
-    getFirstElement(res)
+    getItemById(req, res)
   } else if (req.method === "POST") {
-    let body = null
     req.on("data", (data) => {
       body = JSON.parse(data)
       AddItemToDB(body)
     })
 
     req.on("end", () => {
-      res.end(`todo item '${body.title}' was   added`)
+      res.end(`todo item '${body.title}' was added`)
+    })
+  } else if (req.method === "PUT") {
+    req.on("data", (data) => {
+      body = JSON.parse(data)
+      editElement(body)
+    })
+
+    req.on("end", () => {
+      res.end(`todo item with id='${body.id}' was modified`)
+    })
+  } else if (req.method === "DELETE") {
+    req.on("data", (data) => {
+      body = JSON.parse(data)
+      deleteElement(body)
+    })
+    req.on("end", () => {
+      res.end(`todo item with id='${body.id}' was deleted`)
     })
   }
 })
--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 const http = require("http")
 const app = require("./app")
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200
-//   res.setHeader("Content-Type", "text/plain")
-//   res.end("Hello World")
-// })
-
-// server.listen(8080, () => {
-//   console.log("The server started on port 8080")
-// })
