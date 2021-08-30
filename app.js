c35150e8ffce418570525ef455bc8dbffe030032
lenovo025 <ZhivotenkoAlex>
2021-08-30 14:42:33 +0300

    added CORS headers

--- app.js
+++ app.js
@@ -16,114 +16,19 @@ const { login, refreshTokens, verifyAccess } = require("./helpers/auth")
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader("Content-Type", "application/json")
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", " Authorization")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers)
+    res.end()
+    return
+  }
+
   let body = null
   let pathname = url.parse(req.url, true).pathname
-
-  // switch (pathname && req.method) {
-  //   case "/api/todo" && "GET":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //     })
-  //     req.on("end", async () => {
-  //       const result = await verifyAccess(req, res)
-  //       if (result) {
-  //         getItemById(req, res)
-  //       } else {
-  //         res.statusCode = 401
-  //         res.end("token error")
-  //       }
-  //     })
-  //     break
-
-  //   case "/api/todo" && "POST":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //       const result = await verifyAccess(req, res)
-  //       if (result) {
-  //         AddItemToDB(body)
-  //       } else {
-  //         res.statusCode = 401
-  //         res.end("token error")
-  //       }
-  //     })
-  //     req.on("end", () => {
-  //       res.end(`todo item '${body.title}' was added`)
-  //     })
-  //     break
-
-  //   case "/api/todo" && "PUT":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //       const result = await verifyAccess(req, res)
-  //       if (result) {
-  //         editElement(body)
-  //       } else {
-  //         res.statusCode = 401
-  //         res.end("token error")
-  //       }
-  //     })
-  //     req.on("end", () => {
-  //       res.end(`todo item with id='${body.id}' was modified`)
-  //     })
-  //     break
-
-  //   case "/api/todo" && "DELETE":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //       const result = await verifyAccess(req, res)
-  //       if (result) {
-  //         deleteElement(body)
-  //       } else {
-  //         res.statusCode = 401
-  //         res.end("token error")
-  //       }
-  //     })
-  //     req.on("end", () => {
-  //       res.end(`todo item with id='${body.id}' was deleted`)
-  //     })
-  //     break
-
-  //   case "/auth" && "POST":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //     })
-  //     req.on("end", async () => {
-  //       const result = await addUser(body)
-  //       if (result) {
-  //         res.end("user alredy exist")
-  //       } else {
-  //         res.end(`user was added`)
-  //       }
-  //     })
-  //     break
-
-  //   case "/login" && "POST":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //     })
-  //     req.on("end", async () => {
-  //       const result = await login(body)
-  //       if (result) {
-  //         res.end(JSON.stringify(result))
-  //       } else {
-  //         res.end("Wrong credentials")
-  //       }
-  //     })
-  //     break
-
-  //   case "/refresh-tokens" && "POST":
-  //     req.on("data", async (data) => {
-  //       body = JSON.parse(data)
-  //     })
-  //     req.on("end", async () => {
-  //       const tokens = await refreshTokens(body)
-  //       res.end(JSON.stringify(tokens))
-  //     })
-  //     break
-
-  //   default:
-  //     break
-  // }
 
   if (pathname === "/api/todo" && req.method === "GET") {
     req.on("data", async (data) => {
@@ -149,7 +54,6 @@ const server = http.createServer((req, res) => {
         res.end("token error")
       }
     })
-
     req.on("end", () => {
       res.end(`todo item '${body.title}' was added`)
     })
